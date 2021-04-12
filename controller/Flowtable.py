7df9a30a05c700a347cca745ab71232a05183192
pullself <854997634@qq.com>
2021-04-12 20:24:31 +0800

    Update Flowtable.py
    改变初始化转发流表的生成逻辑，避免重复

--- controller/Flowtable.py
+++ controller/Flowtable.py
@@ -12,12 +12,21 @@ class FlowtableManager(object):
             sw: SimpleSwitchAPI(self.topo.get_thrift_port(sw))
             for sw in self.sw_name
         }
+        self.multicast_table = dict()
 
     def add_forward_table(self):
         host_list = [h for h in self.topo.get_hosts().keys()]
         for src in self.sw_name:
             self.controller[src].table_set_default('ingress.ipv4_c.ipv4',
                                                    'drop', [])
+            direct_sw_list = self.topo.get_switch_connected_to(src)
+            for sw in direct_sw_list:
+                port = self.topo.node_to_node_port_num(src, sw)
+                self.controller[src].table_add(
+                    'egress.mac_c.adjust_mac', 'set_mac', [str(port)], [
+                        str(self.topo.node_to_node_mac(src, sw)),
+                        str(self.topo.node_to_node_mac(sw, src))
+                    ])
             direct_host_list = self.topo.get_hosts_connected_to(src)
             for h in direct_host_list:
                 ip = self.topo.get_host_ip(h)
@@ -26,7 +35,7 @@ class FlowtableManager(object):
                                                'forward', [str(ip) + '/32'],
                                                [str(port)])
                 self.controller[src].table_add(
-                    'egress.mac_c.adjust_mac', 'set_mc', [str(port)], [
+                    'egress.mac_c.adjust_mac', 'set_mac', [str(port)], [
                         str(self.topo.node_to_node_mac(src, h)),
                         str(self.topo.node_to_node_mac(h, src))
                     ])
@@ -39,14 +48,10 @@ class FlowtableManager(object):
                 self.controller[src].table_add('ingress.ipv4_c.ipv4',
                                                'forward', [str(ip) + '/32'],
                                                [str(port)])
-                self.controller[src].table_add(
-                    'egress.mac_c.adjust_mac', 'set_mc', [str(port)], [
-                        str(self.topo.node_to_node_mac(src, path[1])),
-                        str(self.topo.node_to_node_mac(path[1], src))
-                    ])
 
     def add_multicast_table(self):
         for sw in self.sw_name:
+            self.multicast_table.update({sw: {}})
             port = self.topo.get_interfaces_to_port(sw)
             num = len(port) - 1
             if sw + '-cpu-eth0' in port.keys():
@@ -65,6 +70,9 @@ class FlowtableManager(object):
                     port_list = [str(i + 1), str(j + 1)]
                     self.controller[sw].mc_node_create(str(n - 1), port_list)
                     self.controller[sw].mc_node_associate(str(n), str(n - 1))
+                    self.multicast_table[sw].update({
+                        (str(i + 1), str(j + 1)): n
+                    })
                     n += 1
 
     def add_forward_entry(self, sw, ip, port):
